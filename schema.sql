cbf4cb54812a3b72999826fe224dca6b0773ab70
Leonardo Aldarondo <leonardo.aldarondo@gmail.com>
2022-07-22 12:31:05 -0400

    Updating queries.sql, schema.sql retirment_info.

--- schema.sql
+++ schema.sql
@@ -27,17 +27,17 @@ CREATE TABLE dept_manager (
 );
 
 CREATE TABLE salaries (
-   emp_no INT NOT NULL,
-   salary INT NOT NULL,
-   from_date DATE NOT NULL,
-   to_date DATE NOT NULL,
-   FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-   PRIMARY KEY (emp_no)
+  emp_no INT NOT NULL,
+  salary INT NOT NULL,
+  from_date DATE NOT NULL,
+  to_date DATE NOT NULL,
+  FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+  PRIMARY KEY (emp_no)
 );
 
 CREATE TABLE dept_emp (
-   dept_no VARCHAR(4) NOT NULL,
    emp_no INT NOT NULL,
+   dept_no VARCHAR NOT NULL,
    from_date DATE NOT NULL,
    to_date DATE NOT NULL,
    FOREIGN KEY (emp_no) REFERENCES salaries (emp_no),
@@ -46,12 +46,11 @@ CREATE TABLE dept_emp (
 	);
 	
 CREATE TABLE titles (
-   emp_no INT NOT NULL,
-   title VARCHAR NOT NULL,
-   from_date DATE NOT NULL,
-   to_date DATE NOT NULL,
-   FOREIGN KEY (emp_no) REFERENCES salaries (emp_no),
-   PRIMARY KEY (emp_no)
+  emp_no INT NOT NULL,
+  title VARCHAR NOT NULL,
+  from_date DATE NOT NULL,
+  to_date DATE NOT NULL,
+  FOREIGN KEY (emp_no) REFERENCES salaries (emp_no)
 );
 
 select * from departments;
